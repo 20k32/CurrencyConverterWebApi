71f3d9d54e8b6c02314be9b7d697ceba1d065864
20k32 <egormobtel@gmail.com>
2023-07-13 18:22:13 +0300

    added validation rule

---
+++ BaseActions/Queries/GetCurrencyLike/GetCurrencyLikeQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using BaseActions.Queries.GetSpecificCurrency;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace BaseActions.Queries.GetCurrencyLike
+{
+    public class GetCurrencyLikeQueryValidator : AbstractValidator<GetCurrencyLikeQuery>
+    {
+        public GetCurrencyLikeQueryValidator()
+        {
+            RuleFor(x => x.CurrencyName)
+                .NotEmpty()
+                .NotNull()
+                .Length(0, 3);
+        }
+    }
+}
